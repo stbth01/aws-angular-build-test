b3bdc6c50f70a27650cd10678a1607ab9eb14da6
Brian Hunscher <BHunscher@mathematica-mpr.com>
2020-09-01 20:02:11 -0400

    more shenjins

--- build-scripts/code-pipeline-kick.js
+++ build-scripts/code-pipeline-kick.js
@@ -17,44 +17,66 @@ var codebuild = new AWS.CodeBuild();
 //   // an error occurred
 //   else console.log(data.stageStates[0].actionStates); // successful response
 // });
-
-codepipeline.startPipelineExecution(params, function (err, data) {
-  if (err) console.log(err, err.stack);
-  // an error occurred
-  //   else console.log(data); // successful response
-  codepipeline.waitFor("started", {}, function (err, data) {
-    console.log("succeed");
-    console.log(err, data);
+codepipeline
+  .startPipelineExecution(params)
+  .promise()
+  .then((data) => {
+    var param = {
+        pipelineExecutionId: "e2cc5691-201e-40c3-bc1e-e7b920682c71" /* required */,
+        pipelineName: "BuildPipeline" /* required */,
+      };
+      return codepipeline.getPipelineExecution(param).promise();
+  }).then((data) => {
+    
   });
-});
+// codepipeline.startPipelineExecution(params, function (err, data) {
+//   if (err) console.log(err, err.stack);
+//   // an error occurred
+//   else {
+//     console.log("hello", data); // successful response
+//   }
+//   // codepipeline.waitFor("started", {}, function (err, data) {
+//   //   console.log("succeed");
+//   //   console.log(err, data);
+//   // });
+// });
+// var param = {
+//   pipelineExecutionId: "e2cc5691-201e-40c3-bc1e-e7b920682c71" /* required */,
+//   pipelineName: "BuildPipeline" /* required */,
+// };
+// codepipeline.getPipelineExecution(param, function (err, data) {
+//   if (err) console.log(err, err.stack);
+//   // an error occurred
+//   else console.log(data); // successful response
+// });
 
-codepipeline.getPipelineState(params, function (err, data) {
-  if (err) console.log(err, err.stack);
-  // an error occurred
-  else {
-    // console.log(data.stageStates[0].actionStates); // successful response
-    console.log(data.stageStates[1].actionStates); // successful response
-    // var paramscb = {
-    //   ids: [
-    //     /* required */
-    //     data.stageStates[1].latestExecution.externalExecutionId,
-    //     /* more items */
-    //   ],
-    // };
-    // codebuild.batchGetBuildBatches(paramscb, function (err, data) {
-    //   if (err) console.log(err, err.stack);
-    //   // an error occurred
-    //   else console.log(data); // successful response
-    // });
-    // codebuild.
-    // data.stageStates[1].actionStates.waitFor("Succeeded", {}, function (
-    //   err,
-    //   data
-    // ) {
-    //   console.log("sweet");
-    // });
-  }
-});
+// codepipeline.getPipelineExecution(params, function (err, data) {
+//   if (err) console.log(err, err.stack);
+//   // an error occurred
+//   else {
+//     // console.log(data.stageStates[0].actionStates); // successful response
+//     console.log(data.stageStates[1].actionStates); // successful response
+//     // var paramscb = {
+//     //   ids: [
+//     //     /* required */
+//     //     data.stageStates[1].latestExecution.externalExecutionId,
+//     //     /* more items */
+//     //   ],
+//     // };
+//     // codebuild.batchGetBuildBatches(paramscb, function (err, data) {
+//     //   if (err) console.log(err, err.stack);
+//     //   // an error occurred
+//     //   else console.log(data); // successful response
+//     // });
+//     // codebuild.
+//     // data.stageStates[1].actionStates.waitFor("Succeeded", {}, function (
+//     //   err,
+//     //   data
+//     // ) {
+//     //   console.log("sweet");
+//     // });
+//   }
+// });
 
 // codepipeline.waitFor("Failed", {}, function (err, data) {
 //   console.log("failed");
